77202a92261463e6b4bf910569ed104872bcf089
Varad Pai <144313199+Art1cD3vil@users.noreply.github.com>
2025-04-12 22:57:03 +0530

    Initial commit: Blockchain-based crowdfunding platform

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,20 +1,19 @@
 const hre = require("hardhat");
 
 async function main() {
-  // We get the contract to deploy
+  const [deployer] = await hre.ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+
   const CrowdHelp = await hre.ethers.getContractFactory("CrowdHelp");
   const crowdHelp = await CrowdHelp.deploy();
 
   await crowdHelp.deployed();
-
   console.log("CrowdHelp deployed to:", crowdHelp.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error);
+    console.error("Deployment failed:", error);
     process.exit(1);
   });
